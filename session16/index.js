3898648bc3fbb42a540f94dd5a501a47c832dc31
catalinC25 <andreipolic8@gmail.com>
2024-08-19 21:25:15 +0300

    inceput Javascript

---
+++ session16/index.js
@@ -0,0 +1,20 @@
+
+function myProcces(product, slices) {
+    console.log("Product selected " + product);
+    console.log("Product " + product + " sliced in " + slices);
+}
+
+myProcces("Apple", 10)
+
+let myList = {
+    age: 13,
+    name: "Cata"
+}
+
+myList["Adress"] = "Aleea 1"
+
+let myList2 = [1, 2]
+
+myList2.push(3)
+
+console.log(myList2)
