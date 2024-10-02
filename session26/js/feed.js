052d1745c0f7623de2414366bca304346bda469c
sergiu.savin <sergiu.savin@ipsos.com>
2024-10-02 20:21:27 +0300

    Sesiunea 27

--- session26/js/feed.js
+++ session26/js/feed.js
@@ -12,6 +12,10 @@ accountSettingsButton.addEventListener("click", function () {
   }
 });
 
+accountSettingsButton.addEventListener("blur", function () {
+  accountSettingsList.style.display = "none";
+});
+
 logoutButton.addEventListener("click", () => {
   window.open("../login.html", "_self");
 });
@@ -74,13 +78,25 @@ const userCommentList = document.getElementsByClassName("userComments")[0];
 commentMessage.innerText =
   localStorage.getItem("comment") || commentMessage.innerText;
 
+let showComments = true;
+
 commentButton.addEventListener("click", function () {
-  commentInput.focus();
   // commentInput.blur() // reversul functiei focus
+
+  if (showComments) {
+    userCommentList.style.display = "block";
+    commentInput.focus();
+  } else {
+    userCommentList.style.display = "none";
+  }
+
+  showComments = !showComments;
 });
 
 function setComment() {
   const newComment = document.createElement("div");
+
+  const newCommentUsername = Date.now();
   const newCommentMessageContent = `
   <div class="commentContent">
               <div class="profileUserComment">
@@ -90,7 +106,7 @@ function setComment() {
                     alt="user profile"
                     class="profileImage"
                 /></a>
-                <span>User name</span>
+                <span>${newCommentUsername}</span>
               </div>
 
               <div class="userCommentText">
@@ -155,4 +171,111 @@ function addRemoveCommentListeners() {
 
 addRemoveCommentListeners();
 
-// de rezolvat cu local storage pt a afisa corect mesajele salvate
+const infoIcon = document.getElementsByClassName("infoIcon")[0];
+const infoMessage = document.getElementsByClassName("infoMessage")[0];
+
+let infoIconDisplayTimeout;
+
+infoIcon.addEventListener("mouseover", function () {
+  infoIconDisplayTimeout = setTimeout(() => {
+    infoMessage.style.display = "block";
+  }, 1000);
+});
+
+infoIcon.addEventListener("mouseout", function () {
+  clearTimeout(infoIconDisplayTimeout);
+  infoMessage.style.display = "none";
+});
+
+infoIcon.addEventListener("click", () => {
+  if (infoMessage.style.display === "block") {
+    infoMessage.style.display = "none";
+  } else {
+    infoMessage.style.display = "block";
+  }
+});
+
+infoIcon.addEventListener("blur", () => {
+  infoMessage.style.display = "none";
+});
+
+const profileOptionsDropdown = document.getElementsByClassName('profileOptionsDropdown')[0];
+const profileOptionsButton = document.getElementsByClassName('profileOptions')[0];
+
+profileOptionsButton.addEventListener('click', function() {
+  if (profileOptionsDropdown.style.display === 'flex') {
+    profileOptionsDropdown.style.display = 'none'
+  } else {
+    profileOptionsDropdown.style.display = 'flex'
+  }
+})
+
+window.addEventListener('click', (event) => {
+  if (event.target.id !== "removePostButton" && event.target.className !== 'bi bi-three-dots') {
+    profileOptionsDropdown.style.display = 'none'
+  }
+})
+
+// profileOptionsButton.addEventListener('blur', (event) => {
+//   console.dir(document.activeElement)
+//   profileOptionsDropdown.style.display = 'none'
+// })
+
+const removePostButton = document.getElementById('removePostButton');
+
+removePostButton.addEventListener('click', () => {
+  document.getElementsByClassName('post')[0].remove();
+})
+
+const searchInput = document.querySelector('.searchInput');
+
+searchInput.addEventListener('keydown', function(event) {
+  // Filtrare pe FE
+
+  const data = [
+    {
+      username: 'Username 1',
+      date: '32 JUL 2024',
+      likes: 10,
+      shares: 15,
+      comments: [],
+      title: 'Ceva',
+      description: 'Altceva'
+    },
+    {
+      username: 'Username 2',
+      date: '32 JUL 2024',
+      likes: 20,
+      shares: 25,
+      comments: [],
+      title: 'Titlu 2',
+      description: 'Description 2'
+    }
+  ]
+
+  // Ne va returna numai rezultatele in care termenul cautat este 100% identic cu valoarea proprietatii
+  // setTimeout( () => {
+  //   const filtredResults = data.filter( post => post.username === event.target.value)
+  //   console.log(filtredResults)
+  // })
+
+  setTimeout(() => {
+    const filtredResults = data.filter(post => post.username.includes(event.target.value))
+    console.log(filtredResults)
+  })
+
+  // Filtrare pe BE
+  filterData(event.target.value).then( data => {
+    // manipularea datelor si afisarea
+  })
+})
+
+async function  filterData(searchTerm) {
+  const filteredPostUrl = `GET_DATA_URL?searchTerm=${searchTerm}`
+  const response = await fetch(filteredPostUrl)
+
+  return response.json()
+}
+
+
+
