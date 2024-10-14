2fc1c06e2642afffdce80d296fc6f52ae7ef513d
sergiu.savin <sergiu.savin@ipsos.com>
2024-10-14 20:22:01 +0300

    Session 30

---
+++ smart-home-app/src/logic/Features.jsx
@@ -0,0 +1,61 @@
+import Feature from "./Feature";
+import PropTypes from "prop-types";
+
+const Features = ({toggleAction}) => {
+  const FEATURES = [
+    {
+      name: "Toggle Lights",
+      action: "Turn the lights on",
+      state: true,
+      id: 0,
+    },
+    {
+      name: "Toggle AC",
+      action: "Turn the AC on",
+      state: false,
+      id: 1,
+    },
+    {
+      name: "Clean",
+      action: "Start vacuuming",
+      state: true,
+      id: 2,
+    },
+    {
+      name: "Coffee time",
+      action: "Make a coffee",
+      state: true,
+      id: 3,
+    },
+  ];
+
+  const toggleTheAction = (value) => {
+    toggleAction(value)
+  }
+
+  return (
+    <div className="container">
+      {FEATURES.map((feature) => {
+        return (
+          <Feature
+            name={feature.name}
+            action={feature.action}
+            key={feature.id}
+            toggleAction={toggleTheAction}
+          />
+        );
+      })}
+
+      {/* <Feature name={FEATURES[0].name} action={FEATURES[0].action}/>
+      <Feature name={FEATURES[1].name} action={FEATURES[1].action}/>
+      <Feature name={FEATURES[2].name} action={FEATURES[2].action}/>
+      <Feature name={FEATURES[3].name} action={FEATURES[3].action}/> */}
+    </div>
+  );
+};
+
+Features.propTypes = {
+    toggleAction: PropTypes.func.isRequired
+}
+
+export default Features;
