bc45a4bb56f94fce33af153f795f40a298932e64
Valhok99 <valentino.rusu@libero.it>
2024-10-16 18:11:53 +0300

    Modify

--- valentin.rusu/smart-home-app/src/App.jsx
+++ valentin.rusu/smart-home-app/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.scss";
 import Features from "./logic/Features";
 import Light from "./ui/Light";
 import Room from "./ui/Room";
-import Air from "./ui/Air";
 
 function App() {
   let [lightState, setLightState] = useState(false);
@@ -22,8 +21,7 @@ function App() {
       <div className="ui-features">
         <Light lightsOn={lightState} />
         <Room status={1}/>
-        <Air airOn={airState}/>
-      </div> 
+      </div>
 
       <Features toggleAction={toggleTheAction} />
     </div>
